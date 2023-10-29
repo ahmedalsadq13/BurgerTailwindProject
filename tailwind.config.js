79098943db1c5e3e732158c5ab126668a499e98e
Ahmed Elsadek <sadek@innovix.net>
2023-10-29 11:59:01 +0200

    - to production phase.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.js"],
   content: ["./src/**/*.{html,js}"],
   darkMode: "class",
   theme: {
